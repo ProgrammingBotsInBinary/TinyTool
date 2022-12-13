4f07b339cc6f2ab1f7784b8fde0bcb0f80f01b44
ProgrammingBotsInBinary <austinwhall.02@gmail.com>
2022-12-13 15:02:56 -0500

    More Changes

--- app.js
+++ app.js
@@ -59,122 +59,119 @@ app.get('/form', (req, response) => {
 
 app.get('/output', (req, response) => {
 	let subgroup = req.query.subgroup;
-	var FM_parentname = "";
-	var FM_subclass1 = "";
-	var FM_subclass2 = "";
-	var FM_subclass3 = "";
-	var FM_methodname = "";
-	var FM_parentfunction1 = "";
-	var FM_parentfunction2 = "";
-	var FM_Description = "";
-	var AF_abstractName = "";
-	var AF_firstVariant = "";
-	var AF_secondVariant = "";
-	var AF_thirdVariant = "";
-	var AF_firstMethod = "";
-	var AF_secondMethod = "";
-	var AF_thirdMethod = "";
-	var AF_Description = "";
-	if(subgroup == "Factory Method"){
-		FM_parentname = req.query.FM_parent_classname;
-		FM_subclass1 = req.query.FM_subclass_name1;
-		FM_subclass2 = req.query.FM_subclass_name2;
-		FM_subclass3 = req.query.FM_subclass_name3;
-		FM_methodname = req.query.FM_method_name;
-		FM_parentfunction1 = req.query.FM_parent_functionname1;
-		FM_parentfunction2 = req.query.FM_parent_functionname2;
-		FM_Description = req.query.FM_description;
+	// let isSubmit = req.query.submitBtn;
+
+	// if (isSubmit == true) {
+		var FM_parentname = "";
+		var FM_subclass1 = "";
+		var FM_subclass2 = "";
+		var FM_subclass3 = "";
+		var FM_methodname = "";
+		var FM_parentfunction1 = "";
+		var FM_parentfunction2 = "";
+		var FM_Description = "";
+		var AF_abstractName = "";
+		var AF_firstVariant = "";
+		var AF_secondVariant = "";
+		var AF_thirdVariant = "";
+		var AF_firstMethod = "";
+		var AF_secondMethod = "";
+		var AF_thirdMethod = "";
+		var AF_Description = "";
+		var B_classname = "";
+		var B_firstMethod = "";
+		var B_secondMethod = "";
+		var B_thirdMethod = "";
+		var B_fourthMethod = "";
+		var B_fifthMethod = "";
+		var B_sixthMethod = "";
+		var B_Description = "";
+		if(subgroup == "Factory Method"){
+			FM_parentname = req.query.FM_parent_classname;
+			FM_subclass1 = req.query.FM_subclass_name1;
+			FM_subclass2 = req.query.FM_subclass_name2;
+			FM_subclass3 = req.query.FM_subclass_name3;
+			FM_methodname = req.query.FM_method_name;
+			FM_parentfunction1 = req.query.FM_parent_functionname1;
+			FM_parentfunction2 = req.query.FM_parent_functionname2;
+			FM_Description = req.query.FM_description;
 
 
-		var query = "INSERT INTO factorymethodtable (parentClass, subClass1, subClass2, subClass3, sharedFunction, parentFunc1, parentFunc2, descr) VALUES " + 
-		"('" + FM_parentname + "', '" + FM_subclass1 + "', '" + FM_subclass2 + "', '" + FM_subclass3 + "', '" + FM_methodname + "', '" + FM_parentfunction1 + "', '" + FM_parentfunction2 + "', '" + FM_Description + "')";
+			var query = "INSERT INTO factorymethodtable (parentClass, subClass1, subClass2, subClass3, sharedFunction, parentFunc1, parentFunc2, descr) VALUES " + 
+			"('" + FM_parentname + "', '" + FM_subclass1 + "', '" + FM_subclass2 + "', '" + FM_subclass3 + "', '" + FM_methodname + "', '" + FM_parentfunction1 + "', '" + FM_parentfunction2 + "', '" + FM_Description + "')";
 
-		connection.query(query, function(error, data){
-			if(error){
-				throw error;
-			}
-			else{
-				console.log('Success?');
-			}
-		});
+			connection.query(query, function(error, data){
+				if(error){
+					throw error;
+				}
+				else{
+					console.log('Successfully input values into FactoryMethodTable in DB');
+				}
+			});
 
-	}
-	if(subgroup == "Abstract Factory"){
-		AF_abstractName = req.query.AF_classname;
-		AF_firstVariant = req.query.AF_firstvariant;
-		AF_secondVariant = req.query.AF_secondvariant;
-		AF_thirdVariant = req.query.AF_thirdvariant;
-		AF_firstMethod = req.query.AF_firstmethod;
-		AF_secondMethod = req.query.AF_secondmethod;
-		AF_thirdMethod = req.query.AF_thirdmethod;
-		AF_Description = req.query.AF_description;
-
-		var query = "INSERT INTO abstractfactorytable (abstractName, firstVariant, secondVariant, thirdVariant, firstMethod, secondMethod, thirdMethod, descr) VALUES " + 
-		"('" + AF_abstractName + "', '" + AF_firstVariant + "', '" + AF_secondVariant + "', '" + AF_thirdVariant + "', '" + AF_firstMethod + "', '" + AF_secondMethod + "', '" + AF_thirdMethod + "', '" + AF_Description + "')";
-
-		connection.query(query, function(error, data){
-			if(error){
-				throw error;
-			}
-			else{
-				console.log('Success?');
-			}
-		});
-
-
-
-	}
-	// let FM_parentname = req.query.FM_parent_classname;
-	// let FM_subclass1 = req.query.FM_subclass_name1;
-	// let FM_subclass2 = req.query.FM_subclass_name2;
-	// let FM_subclass3 = req.query.FM_subclass_name3;
-	// let FM_methodname = req.query.FM_method_name;
-	// let FM_parentfunction1 = req.query.FM_parent_functionname1;
-	// let FM_parentfunction2 = req.query.FM_parent_functionname2;
-	// let FM_Description = req.query.FM_description;
-	async.series({
-		slider: function(cb){
-			connection.query('SELECT * FROM factorymethodtable', function(err, result, client){
-				cb(err, result);
-			})
-		},
-		new: function(cb){
-			connection.query('SELECT * FROM abstractfactorytable', function(err, result, client){
-				cb(err, result)
-			})
-		}, function(error, results){
-			if(!error){
-				response.render('output', results);
-			}
 		}
+		if(subgroup == "Abstract Factory"){
+			AF_abstractName = req.query.AF_classname;
+			AF_firstVariant = req.query.AF_firstvariant;
+			AF_secondVariant = req.query.AF_secondvariant;
+			AF_thirdVariant = req.query.AF_thirdvariant;
+			AF_firstMethod = req.query.AF_firstmethod;
+			AF_secondMethod = req.query.AF_secondmethod;
+			AF_thirdMethod = req.query.AF_thirdmethod;
+			AF_Description = req.query.AF_description;
 
+			var query = "INSERT INTO abstractfactorytable (abstractName, firstVariant, secondVariant, thirdVariant, firstMethod, secondMethod, thirdMethod, descr) VALUES " + 
+			"('" + AF_abstractName + "', '" + AF_firstVariant + "', '" + AF_secondVariant + "', '" + AF_thirdVariant + "', '" + AF_firstMethod + "', '" + AF_secondMethod + "', '" + AF_thirdMethod + "', '" + AF_Description + "')";
+
+			connection.query(query, function(error, data){
+				if(error){
+					throw error;
+				}
+				else{
+					console.log('Successfully input values into AbstractFactoryTable in DB');
+				}
+			});
+		}
+		if(subgroup == "Builder"){
+			B_classname = req.query.B_classname;
+			B_firstMethod = req.query.B_firstmethod;
+			B_secondMethod = req.query.B_secondmethod;
+			B_thirdMethod = req.query.B_thirdmethod;
+			B_fourthMethod = req.query.B_fourthmethod;
+			B_fifthMethod = req.query.B_fifthmethod;
+			B_sixthMethod = req.query.B_sixthmethod;
+			B_Description = req.query.B_description;
+
+			var query = "INSERT INTO buildertable (className, firstMethod, secondMethod, thirdMethod, fourthMethod, fifthMethod, sixthMethod, descr) VALUES " + 
+			"('" + B_classname + "', '" + B_firstMethod + "', '" + B_secondMethod + "', '" + B_thirdMethod + "', '" + B_fourthMethod + "', '" + B_fifthMethod + "', '" + B_sixthMethod + "', '" + B_Description + "')";
+
+			connection.query(query, function(error, data){
+				if(error){
+					throw error;
+				}
+				else{
+					console.log('Successfully input values into BuilderTable in DB');
+				}
+			});
+
+		}
+	//}
+
+	connection.query('SELECT * FROM factorymethodtable', function(err, result, client){
+		var factoryResult = result;
+		connection.query('SELECT * FROM abstractfactorytable', function(err, result, client){
+			var abstractResult = result;
+			connection.query('SELECT * FROM buildertable', function(err, result, client){
+				var builderResult = result;
+				response.render('output', { 	
+						factoryResult: factoryResult, 
+						abstractResult: abstractResult, 
+						builderResult: builderResult
+				});
+			});
+		});
 	});
 
-
-	//  connection.query('SELECT * FROM factorymethodtable', function (err, result) {
-	// 	if (err) throw err;
-
-	// 	response.render('output', {data: result});
-	//   });
-	//   connection.query('SELECT * FROM abstractfactorytable', function (err, result) {
-
-	// 	if (err) throw err;
-
-	// 	response.render('output', {data1: result});
-	//   });
-
-	// console.log(subgroup);
-	// response.render('output', {
-	// 	subgroup: subgroup,
-	// 	FM_parentname: FM_parentname,
-	// 	FM_subclass1: FM_subclass1,
-	// 	FM_subclass2: FM_subclass2,
-	// 	FM_subclass3: FM_subclass3,
-	// 	FM_methodname: FM_methodname,
-	// 	FM_parentfunction1: FM_parentfunction1,
-	// 	FM_parentfunction2: FM_parentfunction2,
-	// 	FM_Description: FM_Description
-	// });
 });
 
 app.get('/', (req, response) => {
